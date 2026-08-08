57845b1bb004bfb631db64538b0c0a851b568cf2
suja0981 <147859989+suja0981@users.noreply.github.com>
2026-08-08 23:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const list=document.getElementById("infi-list");
 
+for(let i=1;i<=10;i++){
+    addItem(i);
+}
+
+function addItem(){
+	const li=document.createElement("li");
+    li.textContent = `${number}`;
+    list.appendChild(li);
+}
+window.addEventListener("scroll", function () {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10) {
+        const currentCount = list.children.length;
+
+        addItem(currentCount + 1);
+        addItem(currentCount + 2);
+    }
+});
